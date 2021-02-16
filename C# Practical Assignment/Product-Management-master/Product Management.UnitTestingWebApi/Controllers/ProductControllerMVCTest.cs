3849127de7ad6ae93d3ba6be17ec0f2b7f54015a
Rikin Patel <40711523+Riki0305@users.noreply.github.com>
2021-02-16 08:48:15 +0530

    Add files via upload

---
+++ C# Practical Assignment/Product-Management-master/Product Management.UnitTestingWebApi/Controllers/ProductControllerMVCTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Product_Management.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Product_Management.UnitTestingWebApi.Controllers
+{
+    [TestClass]
+    public class ProductControllerMVCTest
+    {
+        [TestMethod]
+        public void ProductIndexPageTesting()
+        {
+            //Arrange
+            ProductController controller = new ProductController();
+            FormCollection form = new FormCollection();
+            form["searchBy"] = "Name";
+            form["search"] = "O";
+
+            //Act
+            var result = controller.Index(1,"Name",form,"Name","O") as ActionResult;
+
+
+            //Assert
+
+            Assert.IsNotNull(result);
+            
+        }
+
+        [TestMethod]
+        public void ProductAddorEditPage()
+        {
+            //Arrange
+            ProductController controller = new ProductController();
+            
+
+            //Act
+            var result = controller.AddorEdit() as ActionResult;
+
+
+            //Assert
+
+            Assert.IsNotNull(result);
+
+        }
+
+
+    }
+}
---
+++ C# Practical Assignment/Product-Management-master/Product Management.UnitTestingWebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Product Management.UnitTestingWebApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Product Management.UnitTestingWebApi")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41caaca2-64a4-4ec8-b882-a48feb849eb8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
